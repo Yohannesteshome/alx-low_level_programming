248f364f25dd2a5c721b13cd6bafe435b3247d13
yohannesteshome <yohannes.teshome.92@gmail.com>
2022-06-03 14:53:43 -0700

    Task 0. Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,23 +2,22 @@
 #include <time.h>                                                                                                                   
 #include <stdio.h>                                                                                                                  
 /**                                                                                                  
- *
+ i
  * * main - Prints if number is positive, zero or negative                                            *
  * *  Return: Always (Success)                                                                     
  *  
- */                                                                                                                              
+ **/                                                                                                                              
 int main(void)                                                                                                                      
-{                                                                                                                                   
-	        int n;                                                                                                                      
-		                                                                                                                                   
+{                                                                                                                                  
+	        int n;                                                                                                                     		                                                                                                                                   
 		        srand(time(0));                                                                                                             
 			        n = rand() - RAND_MAX / 2;                                                                                                  
-				        if (n > 0)                                                                                                                  
-						                                                                                                                                    
+				        if (n > 0)                                                                                                                 
+						                                                                                                                                   
 					{                                                                                                                                   
-						        printf("%d is positive\n", n);                                                                                              
+						        printf("%d is positive\n", n);                                                                                             
 					}                                                                                                                                   
-					                                                                                                                                    
+					                                                                                                                                   
 					        else if (n == 0)                                                                                                            
 						{                                                                                                                                   
 							 printf("%d is zero\n", n);                                                                                                  
