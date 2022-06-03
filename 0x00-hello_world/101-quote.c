f2388615e74ecc8c38407f6df2e5fd6bf6ebb6da
yohannesteshome <yohannes.teshome.92@gmail.com>
2022-06-03 00:15:40 -0700

    Task 8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,15 @@
-​#include <stdio.h>
-
-/**
+#include <unistd.h>                                                                                                                     
+/**                                                                                
  *
- * * main - Entry point
+ * * main - Entry point                                                                               *
+ * *                                                                                                  *
+ * * Return: Always 1 (Success)                                                                    
  *
- * *
- *
- * * Return: Always 1 (Success)
- *
- * */
-
-int main(void)
-
-{
-
-	char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-	fprintf(stderr, "%s\n", n);
-
-	return (1);
-
+ */                                                                                                                                      
+int main(void)                                                                                                                          
+{                                                                                                                                       
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";                                                         
+	                                                                                                                                        
+	write(2, str1, 59);                                                                                                                     
+	return (1);                                                                                                                             
 }
