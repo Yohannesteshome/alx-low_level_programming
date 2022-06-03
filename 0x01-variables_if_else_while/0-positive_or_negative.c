854dfd2cdf9b6f2e52c930781fbd60ad29266017
yohannesteshome <yohannes.teshome.92@gmail.com>
2022-06-03 15:32:40 -0700

    Task 0. Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,30 +1,32 @@
-#include <stdlib.h>                                                                                                                 
-#include <time.h>                                                                                                                   
-#include <stdio.h>                                                                                                                  
-/**                                                                                                  
- i
- * * main - Prints if number is positive, zero or negative                                            *
- * *  Return: Always (Success)                                                                     
- *  
- **/                                                                                                                              
-int main(void)                                                                                                                      
-{                                                                                                                                  
-	        int n;                                                                                                                     		                                                                                                                                   
-		        srand(time(0));                                                                                                             
-			        n = rand() - RAND_MAX / 2;                                                                                                  
-				        if (n > 0)                                                                                                                 
-						                                                                                                                                   
-					{                                                                                                                                   
-						        printf("%d is positive\n", n);                                                                                             
-					}                                                                                                                                   
-					                                                                                                                                   
-					        else if (n == 0)                                                                                                            
-						{                                                                                                                                   
-							 printf("%d is zero\n", n);                                                                                                  
-						}                                                                                                                                   
-					else                                                                                                                                
-					{                                                                                                                                  
-						        printf("%d is negative\n", n);                                                                                             
-					}
-					return (0);                                                                                                                        
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *
+ * * main - Entry point
+ *
+ * *
+ *
+ * * Return: Always (Success)
+ *
+ */
+int main(void)
+{
+	int n, digit;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	digit = n % 10;
+	if (digit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
+	}
+	else if (digit == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, digit);
+	}
+	else if (digit < 6 && digit != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
+	}
+	return (0);
 }
