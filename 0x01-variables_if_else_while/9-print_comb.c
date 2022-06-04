23ac7fb2c5135486458d2c1d67596f496e5d8130
yohannesteshome <yohannes.teshome.92@gmail.com>
2022-06-04 03:56:31 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * * main - Entry point
+ *
+ * *
+ *
+ * * Return: Always (Success)
+ *
+ */
+int main(void)
+{
+	int t;
+
+	for (t = '0'; t <= '9'; t++)
+	{
+		putchar (t);
+		if (t != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
