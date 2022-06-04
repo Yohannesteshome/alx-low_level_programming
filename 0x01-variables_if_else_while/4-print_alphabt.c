c802093a56dda3e9beadf9940483483369a643ec
yohannesteshome <yohannes.teshome.92@gmail.com>
2022-06-04 03:34:39 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * * main - Entry point
+ *
+ * *
+ *
+ * * Return: Always (Success)
+ *
+ */
+int main(void)
+{
+	char lower = 'a';
+
+	while (lower <= 'z')
+	{
+		if ((lower != 'e') && (lower != 'q'))
+		{
+			putchar (lower);
+		}
+		lower++;
+	}
+	putchar('\n');
+	return (0);
+}
