9b9d6b3e696ee7416b2ca324b738bdec4463757b
yohannesteshome <yohannes.teshome.92@gmail.com>
2022-06-04 03:49:07 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * * main - Entry point
+ *
+ * *
+ *
+ * * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char lower = 'z';
+
+	while (lower >= 'a')
+	{
+		putchar (lower);
+		lower--;
+	}
+	putchar ('\n');
+	return (0);
+}
