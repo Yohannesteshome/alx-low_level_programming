c597033ab7f60a7787f102496dce31d916e654ad
yohannesteshome <yohannes.teshome.92@gmail.com>
2022-06-16 14:03:57 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,33 @@
 #include "main.h"
-#include <stdio.h>
-
+#include<stdio.h>
 /**
-  * cap_string - ...
-  * @s: ...
-  *
-  * Return: char value
-  */
-char *cap_string(char *s)
+ * *cap_string - function capitalizes all words of a string
+ *
+ *@n: char * pointer
+ *
+ * Return: char *
+ */
+
+char *cap_string(char *n)
 {
-	int a = 0, i;
-	int cspc = 13;
-	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+	int count;
+	int count2;
+	char *x = " .,{}()\n\t\"?!";
 
-	while (s[a])
-i;	{
-		i = 0;
-
-		while (i < cspc)
+	for (count = 0; *(n + count) != '\0'; count++)
+	{
+		for (count2 = 0; *(x + count2) != '\0'; count2++)
 		{
-			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
-				s[a] -= 32;
-
-			i++;
+			if (*(n + count - 1) == *(x + count2) && *(n + count) >=
+			    'a' && *(n + count) <= 'z')
+			{
+				*(n + count) = ('A' - 'a') + *(n + count);
+			}
+			else if ((count == 0) && *(n + count) >= 'a' && *(n + count) <= 'z')
+			{
+				*(n + count) = ('A' - 'a') + *(n + count);
+			}
 		}
-
-		a++;
 	}
-
-	return (s);
+	return (n);
 }
